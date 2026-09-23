2a2a7fa2d31ee6fe1f2363f692ed3e809d62ed22
xinhong77 <Xinn01729@gmail.com>
2026-09-23 11:53:33 +0800

    test: require public demo link in README

--- tests/testthat/test-deployment.R
+++ tests/testthat/test-deployment.R
@@ -2,6 +2,14 @@ library(testthat)
 
 app_dir <- normalizePath(file.path(testthat::test_path(), "..", ".."), mustWork = TRUE)
 
+test_that("README links readers to the public Shinygantt demo", {
+  readme_path <- file.path(app_dir, "README.md")
+  readme <- paste(readLines(readme_path, warn = FALSE), collapse = "\n")
+
+  expect_match(readme, "## Live demo")
+  expect_match(readme, "https://01a0cc62-dd4e-09eb-da14-0b1d9c01126e.share.connect.posit.cloud/")
+})
+
 test_that("Connect Cloud deployment manifest is present and records the app dependencies", {
   manifest_path <- file.path(app_dir, "manifest.json")
 
